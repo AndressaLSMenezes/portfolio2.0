13189905472807046ec57eb11170e250b42c27fa
Andressa <andressalsmenezes@gmail.com>
2023-05-26 17:51:16 -0300

    fix: about me section height

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,6 +1,6 @@
 export default function AboutMe() {
   return (
-    <section className="section h-[90vh]" id="aboutMe">
+    <section className="section" id="aboutMe">
       <h2 className="section-title"># Sobre mim</h2>
 
       <p className="about-me-p">
